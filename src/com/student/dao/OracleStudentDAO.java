42efd0508a33ba36909eb3af52935c919df73abe
Pratyay Ghosh <pratyayghosh21012004.com>
2026-02-28 15:20:54 +0530

    added util package

--- src/com/student/dao/OracleStudentDAO.java
+++ src/com/student/dao/OracleStudentDAO.java
@@ -7,6 +7,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import com.student.model.Student;
+import com.student.util.DBConnection;
 
 public class OracleStudentDAO implements StudentDAO {
 
--- src/com/student/util/DBConnection.java
+++ src/com/student/util/DBConnection.java
@@ -1,19 +1,19 @@
-package com.student.dao;
+package com.student.util;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/studentdb";
-    private static final String USER = "root";
-    private static final String PASSWORD = "root";
+    private static final String URL = "jdbc:postgresql://localhost:5432/studentdb";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "postgres";
 
     public static Connection getConnection() throws SQLException {
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
+            Class.forName("org.postgresql.Driver");
         } catch (ClassNotFoundException e) {
-            System.out.println("MySQL JDBC Driver not found!");
+            System.out.println("PostgreSQL JDBC Driver not found!");
             e.printStackTrace();
         }
         return DriverManager.getConnection(URL, USER, PASSWORD);
